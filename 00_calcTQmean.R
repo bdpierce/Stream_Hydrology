19c10473f9c48fc23bfefbac731e06230e43d324
bdpierce <bdpierce01@gmail.com>
2026-05-21 18:50:41 -0700

    init

---
+++ 00_calcTQmean.R
@@ -0,0 +1,7 @@
+# ------------------------------------------------------------------------------
+# Purpose: Creates a function to calculate TQmean
+# Author: Brianna Pierce
+# Email: bpierce@bellevuewa.gov
+# Created: 2026-05-21
+# Revised: 2026-05-21
+# ------------------------------------------------------------------------------
