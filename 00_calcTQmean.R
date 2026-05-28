bc709de723a54aa93e3f93f964bd67b739c022d5
bdpierce <bdpierce01@gmail.com>
2026-05-28 11:14:02 -0700

    Update required packages

--- 00_calcTQmean.R
+++ 00_calcTQmean.R
@@ -2,47 +2,41 @@
 # Purpose: Creates a function to calculate TQmean
 # Author: Brianna Pierce
 # Email: bpierce@bellevuewa.gov
-# Created: 2026-05-21
-# Revised: 2026-05-26
 # ------------------------------------------------------------------------------
 
-# For development only: get the data
-daily <- getStage(aggregation_unit = "day")
-View(daily)
+# *** TQmean is the fraction of time during a water year that the daily average 
+#     flow rate is greater than the annual average flow rate of that year.
 
-# *** TQmean is based off of the daily average
+
+# For development only: get the data
+# source("00_getStage.R")
+# daily <- getStage(aggregation_unit = "day")
+# daily_data <- daily
+# View(daily_data)
 
 # Required Packages ------------------------------------------------------------
 require(dplyr)
-require(tidyr)
-require(lubridate)
 
 # Create a function to calculate the TQmean ------------------------------------
-calc.TQmean <- function(daily_data) {
+calcTQmean <- function(daily_data) {
   
+  # Calculate the mean stage per WY per gauge
+  daily_data <- daily_data %>% 
+    group_by(Gauge, WY) %>% 
+    mutate(WY_avg_stage = mean(Stage_ft, na.rm = T))
+  
+  # Flag values greater than the annual mean
+  daily_data$tqm_flag <- if_else(daily_data$Stage_ft > daily_data$WY_avg_stage, 1, 0)
+  
+  # Calculate the proportion of daily averages over the mean daily average stage
+  # And include a summary of sample completeness
   TQmean <- daily_data %>% 
-    group_by(WY) %>% 
-    mutate(WY_avg_stage = mean) #....ok I have multiple columns for each gauge
-  # I think I should edit the getStage function to keep it pivoted long. 
-  # Then group by gauge and WY.
-  # Pick back up here .............................
+    summarize(TQmean = sum(tqm_flag, na.rm=T)/sum(!is.na(Stage_ft)), 
+              SampledDays = sum(!is.na(Stage_ft)),
+              PercentComplete = round(sum(!is.na(Stage_ft))/yday(paste0(max(WY), "-12-31"))*100, 1) )
   
-  
-  
-  
-  # step 1: pull out data only for the water year
-  daily.data <- daily.data %>%
-    mutate(date = as.Date(date, format = "%Y-%m-%d")) %>%
-    filter(date >= start.date & date <= end.date) %>%
-    arrange(date)
-  # step 2 Compute and append annual average reach depth to each row
-  daily.data <- daily.data %>%
-    mutate(mean.ard = mean(daily.ard.ft)) 
-  
-  # step 3 create logical value representing daily average depth > annual average depth (i.e. tqm)
-  daily.data$tqm <- ifelse(daily.data$mean.ard > daily.data$daily.ard.ft, 1, 0)
-  
-  # step 4 compute proportion of time over mean daily average reach depth
-  TQmean <- sum(daily.data$tqm)/length(daily.data$tqm)
   return(TQmean)
 }
+
+# Test the function
+#calcTQmean(daily_data)
